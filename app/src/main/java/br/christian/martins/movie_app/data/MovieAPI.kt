96ec9adf83d5bb725968fbba5a6b4aa1215236b0
christianrmartins <christian.martins@trilogo.com.br>
2024-02-12 09:47:34 -0300

    get-movies-list fix change name

--- app/src/main/java/br/christian/martins/movie_app/data/MovieAPI.kt
+++ app/src/main/java/br/christian/martins/movie_app/data/MovieAPI.kt
@@ -17,25 +17,17 @@ class MovieAPI {
         }
     }
 
-    suspend fun getMoviesT(page: Int = 1) = get("3/movie/now_playing?language=en-US&page=$page")
+    suspend fun getMovies(page: Int = 1) = get("3/movie/now_playing?language=en-US&page=$page")
 
-    suspend fun getMovies(page: Int = 1): List<Movie> {
-        val response = get("3/movie/now_playing?language=en-US&page=$page")
-        println(response.status)
-        return listOf()
-    }
+//    suspend fun getMovies(page: Int = 1): List<Movie> {
+//        val response = get("3/movie/now_playing?language=en-US&page=$page")
+//        println(response.status)
+//        return listOf()
+//    }
 
     private suspend fun get(path: String) = httpClient.get(url + path)
 
     fun dispose() {
         httpClient.close()
     }
-
-//    private suspend fun getMovies(page: Int) {
-//        val client = HttpClient(CIO)
-//        val response: HttpResponse = client.get("$url\"3/movie/now_playing?language=en-US&page=$page\"")
-//        println(response.status)
-//        client.close()
-//    }
-
 }
